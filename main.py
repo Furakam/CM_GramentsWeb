1ee5f92d24cf6dfab62dce058a1f91f660c55b95
nito543 <nito987321@gmail.com>
2021-05-17 20:42:37 -0500

    Le pusimos el pie de pagina al nav y su respectivo css

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('/base/nav.html')
+ 
+@app.route('/index')
+def practica():
+  return render_template('index.html')
 
 
 if __name__ == "__main__":
